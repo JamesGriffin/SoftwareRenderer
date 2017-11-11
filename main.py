8269f7784a046ae62aa64a2f0c29f3e74a2cbb05
James Griffin <james@vogsphere.net>
2017-11-11 20:56:58 +0000

    Added FPS counter and some useful methods to renderer

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from renderer import Renderer
 WIDTH = 800
 HEIGHT = 600
 WINDOW_TITLE = "Software Renderer"
+MAX_FRAMERATE = 60
 
 if __name__ == "__main__":
     renderer = Renderer(WIDTH, HEIGHT, WINDOW_TITLE)
 
     while True:
-        renderer.clear()
-        renderer.process_events()
+        if renderer.get_delta() >= (1.0/MAX_FRAMERATE):
+            renderer.process_events()
+            renderer.clear()
+            renderer.render_fps()
+            renderer.update()
--- renderer.py
+++ renderer.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import pygame
 
 class Renderer:
@@ -15,14 +16,29 @@ class Renderer:
         pygame.display.set_caption(title)
         self.surface = pygame.display.set_mode((width, height))
 
+        # Time
+        self.last_update = time.time()
+
+        # Font
+        self.font = pygame.font.SysFont("Arial", 12)
+
+    # Calculate time delta since last update
+    def get_delta(self):
+        return time.time() - self.last_update
+
     # Clear screen and fill with color (default is grey)
     def clear(self, colour=(128, 128, 128)):
         self.surface.fill(colour)
-        self.update()
+
+    # Render FPS counter
+    def render_fps(self):
+        text_surface = self.font.render(("%d FPS" % (1 / self.get_delta())), True, (0, 0, 0))
+        self.surface.blit(text_surface, (8, 8))
 
     # Update display
     def update(self):
         pygame.display.update()
+        self. last_update = time.time()
 
     # Process pygame events
     def process_events(self):
